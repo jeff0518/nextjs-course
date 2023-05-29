bddbafd0fa0da2865668424e007b705cbdf0912f
jeff0518 <cycivs07@icloud.com>
2023-05-29 21:03:32 +0800

    feat: add new-meetup

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList"
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image: "https://i.imgur.com/WMIRarf.jpg",
+    address: "10019, New York, United States",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image: "https://i.imgur.com/WMIRarf.jpg",
+    address: "10019, New York, United States",
+    description: "This is a second meetup!",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage
