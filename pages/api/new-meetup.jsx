ed3a20320303925036fcedbab945e462aa9608ce
jeff0518 <cycivs07@icloud.com>
2023-06-09 16:35:35 +0800

    feat : add

--- pages/api/new-meetup.jsx
+++ pages/api/new-meetup.jsx
@@ -4,12 +4,16 @@ async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body
 
-    const {title, image, address, description} = data;
+    // const {title, image, address, description} = data;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://cycivs07:ahRvXkeYVUW3HBL2@cluster0.w6rfo7r.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://cycivs07:ahRvXkeYVUW3HBL2@cluster0.w6rfo7r.mongodb.net/meetups?retryWrites=true&w=majority"
     );
 
     const db = client.db()
+
+    const meetupsCollection = db.collection('meetups')
+
+    meetupsCollection.insertOne({data})
   }
 }
