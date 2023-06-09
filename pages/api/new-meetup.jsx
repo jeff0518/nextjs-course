204c641072a7466fbc3fbb5ab2c037de1ac546a5
jeff0518 <cycivs07@icloud.com>
2023-06-10 01:43:39 +0800

    fix: api router bug

--- pages/api/new-meetup.jsx
+++ pages/api/new-meetup.jsx
@@ -1,19 +1,28 @@
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
-  if (req.method === 'POST') {
-    const data = req.body
+
+  if (req.method === "POST") {
+    const data = req.body;
 
     // const {title, image, address, description} = data;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://cycivs07:ahRvXkeYVUW3HBL2@cluster0.w6rfo7r.mongodb.net/meetups?retryWrites=true&w=majority"
+      "mongodb+srv://cycivs07:nOuSNsfYTvTFQHiZ@cluster0.w6rfo7r.mongodb.net/meetups?retryWrites=true&w=majority"
     );
 
-    const db = client.db()
+    const db = client.db();
 
-    const meetupsCollection = db.collection('meetups')
+      const meetupsCollection = db.collection("meetups");
 
-    meetupsCollection.insertOne({data})
+      const result = await meetupsCollection.insertOne(data);
+
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({ message: "Meetup inserted!" });
   }
 }
+
+export default handler;
